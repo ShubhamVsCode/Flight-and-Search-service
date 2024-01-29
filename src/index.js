f8abef8b45717043d6abe982adf7eb944aacf0dd
Shubham <shubhamvscode@gmail.com>
2024-01-30 01:20:24 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
